783e20ca8969c9215a4be7eb0714b9fbd36fa69b
Nikanora <37082879+Nikanorra@users.noreply.github.com>
2020-10-10 22:32:40 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ $(function(){
   $('.collapse').on('hide.bs.collapse', function(){
     $(this).prev().find('.fas').removeClass().addClass('fas fa-plus-circle')
   });
+  
+  new WOW().init();
 });
 
 
